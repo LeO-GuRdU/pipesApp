9b8b7e908b6fda079ca4d677f1d85d02579e1729
“Leonardo <leonardo.gurdulich@halopowered.com>
2022-12-08 15:03:26 -0300

    Pipes No Comunes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRouterModule } from './app-router.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+//Módulos perzonalizados
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el locale de la app
+import localEs from '@angular/common/locales/es-AR';
+import localBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localEs );
+registerLocaleData( localBr );
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    PrimeNgModule,
+    SharedModule,
+    VentasModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  exports: [
+    PrimeNgModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent {
+
+  nombre : string = "lEonArdo gurDuLich";
+
+  fecha : Date = new Date();
+
+}
